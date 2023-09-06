9943f3460246d939b98c9488ed4ebc4d2c31fff6
HanbinLee <92098748+lesserpanda1@users.noreply.github.com>
2023-09-06 17:39:00 -0400

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,28 +1,10 @@
 package lab_assignment1;
 import java.time.LocalDate;
 import java.time.Year;
-//23W CST8284
-//Assignment 1: Patient.java
-/**
- * File name: Patient.java
- * Author: Hanbin Lee
- * Course: CST8284 - OOP
- * Assignment: Lab Assignment 1
- * Professor: Hesham Saadawi
- * Purpose: Update the source code provided to work as instructed.
- * Class list: Patient
- */
-
 
 //This system maintains important health information about a patient.
-//Note: This class assumes values passed to the set methods are correct.
-// REVIEW THIS FILE AND ASSIGNMENT INSTRUCTIONS CAREFULLY BEFORE YOU START OFF!!
+
 public class Patient {
-
-//TO DO 1: INCLUDE YOUR ATTRIBUTES HERE. REMEMBER TO CHECK THE UML CLASS DIAGRAM TO INCLUDE ALL ATTRIBUTES LISTED AND THEIR TYPES.
-
-//START CODE
-//END CODE
 	
 	/**
 	 * patient's first name for recording health data
@@ -64,10 +46,6 @@ public class Patient {
 	 */
 	private double weight;
 	
-	
-// TO DO 2: INCLUDE YOUR CONSTRUCTOR HERE 
-//START CODE
-	
 	/**
 	 * This is the default constructor without parameters
 	 */
@@ -97,12 +75,7 @@ public class Patient {
         this.height = height;
         this.weight = weight;
 	}
-//END CODE
-	
 
-
-//TO DO 3: INCLUDE YOUR SET AND GET METHODS HERE. CHECK THE UML CLASS TO ENSURE ALL ARE COMPLETE. SOME ARE ALREADY PROVIDED FOR YOU BELOW.
-	
 	/**
 	 * Returns the first name of the patient
 	 * @return firstName the first name of the patient
@@ -231,8 +204,6 @@ public class Patient {
 		this.weight = weight;
 	}
 
-// THIS RETURNS THE PATIENT'S Body Mass Index (BMI) AND IT IS PROVIDED HERE FOR YOU. DO NOT CHANGE THIS PIECE OF CODE!
-	
 	/**
 	 * Calculates patient's BMI value by following the formula below
 	 * @return BMI value
@@ -241,8 +212,6 @@ public class Patient {
 	   return (getWeight() * 703) / (getHeight() * getHeight());
 	} 
 
-// TO DO 4: THIS PORTION OF CODE SHOULD DISPLAY ALL THE PATIENT'S HEALTH DATA. DO NOT MISS ANY DATA / INFORMATION
-	
 	/**
 	 * Age() method calculates patient's age by subtracting the year of birth from current year
 	 * @return age the age of the patient
@@ -272,18 +241,16 @@ public class Patient {
 	 * This method records patient's health data and displays whole input data.
 	 */
 	public void displayMyHealthData() { 
-	
-	//TO DO 5: HINT: YOU MUST USE System.out.printf TO SHOW ALL HEALTH DATA. DO NOT MISS ANY ITEM.
 	   
-	// START CODE
-	   System.out.printf("First name: %s %n", firstName);
-	   System.out.printf("Last name: %s %n", lastName);
-	   System.out.printf("Gender: %s %n", gender);
-	   System.out.printf("Year of birth, month of birth, day of birth: %04d-%02d-%02d %n", birthYear, birthMonth, birthDay);
-	   System.out.printf("Height: %.1f inches %n", height);
-	   System.out.printf("Weight: %.1f pounds %n", weight);
-       System.out.printf("Age: %d years old %n", Age());
-       System.out.printf("Maximum heart rate: %d %n", MaximumHeartRate());
+	
+	System.out.printf("First name: %s %n", firstName);
+	System.out.printf("Last name: %s %n", lastName);
+	System.out.printf("Gender: %s %n", gender);
+	System.out.printf("Year of birth, month of birth, day of birth: %04d-%02d-%02d %n", birthYear, birthMonth, birthDay);
+	System.out.printf("Height: %.1f inches %n", height);
+	System.out.printf("Weight: %.1f pounds %n", weight);
+	System.out.printf("Age: %d years old %n", Age());
+	System.out.printf("Maximum heart rate: %d %n", MaximumHeartRate());
        
        double minTarget = MaximumHeartRate() / 2;
        double maxTarget = MaximumHeartRate() * 0.85;
@@ -292,18 +259,15 @@ public class Patient {
        double bmi = getBMI();
        System.out.printf("BMI: %.1f %n", bmi);
        System.out.println();
-	//END CODE
-	   
-	//DO NOT MODIFY THIS PROTION OF CODE. IT SHOULD PRINT AS IT IS - ALREADY PROVIDED FOR YOU!!
 	
-	   System.out.println("BMI VALUES");
-	   System.out.println("Underweight: less than 18.5");
-	   System.out.println("Normal:      between 18.5 and 24.9");
-	   System.out.println("Overweight:  between 25 and 29.9");
-	   System.out.println("Obese:       30 or greater");   
+	System.out.println("BMI VALUES");
+	System.out.println("Underweight: less than 18.5");
+	System.out.println("Normal:      between 18.5 and 24.9");
+	System.out.println("Overweight:  between 25 and 29.9");
+	System.out.println("Obese:       30 or greater");   
 	} 
 
-} // end class Patient
+}
 
 
 
